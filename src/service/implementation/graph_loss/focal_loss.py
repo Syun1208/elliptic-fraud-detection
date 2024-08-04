424e678af988244edd69aa8091283cd525314d98
Syun1208 <phamlong12082001@gmail.com>
2024-08-04 11:24:54 +0700

    afgagaga

--- src/service/implementation/graph_loss/focal_loss.py
+++ src/service/implementation/graph_loss/focal_loss.py
@@ -26,10 +26,10 @@ class FocalLoss(nn.Module):
             input = input.view(input.size(0),input.size(1),-1)  # N,C,H,W => N,C,H*W
             input = input.transpose(1,2)    # N,C,H*W => N,H*W,C
             input = input.contiguous().view(-1,input.size(2))   # N,H*W,C => N*H*W,C
-        target = target.view(-1, 1).squeeze(1)
+        target = target.view(-1, 1)
 
         logpt = F.log_softmax(input)
-        logpt = logpt.gather(1,target)
+        logpt = logpt.gather(0,target)
         logpt = logpt.view(-1)
         pt = Variable(logpt.data.exp())
 
